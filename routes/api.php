117cf74717aa157b24e26c1122f1911f3217df08
versatile153 <ceze46321@gmail.com>
2024-12-26 12:36:27 +0100

    supremeweb commit

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// // API routes for your wallet system
+// // API routes wallet system
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/wallets', [WalletController::class, 'index']);
 Route::get('/wallets/{id}', [WalletController::class, 'show']);
